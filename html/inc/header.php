a29a4c5f6790280b2dede12acddd74d69731ee11
gocab <gocab@www.go-cab.com>
2012-05-14 01:22:51 -0400

    added design, php started

---
+++ html/inc/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>GoCab - Start</title>
+<link rel="stylesheet" href="css/gocab.css" />
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.0-rc.1/jquery.mobile-1.1.0-rc.1.min.css" />
+<link rel="stylesheet" href="css/jquery.mobile.datebox.css" />
+<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.1.0-rc.1/jquery.mobile-1.1.0-rc.1.min.js"></script>
+<script src="js/jquery.mobile.datebox.js"></script>
+<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDS5FvKukU8JDBpmnQgtXQtA-8RzqdLV8Q&sensor=true"></script>
+<script type="text/javascript" src="js/gocab.js"></script>
+</head>
---
+++ html/index.php
@@ -0,0 +1,20 @@
+<?
+require('inc/header.php');
+?>
+<body class="index">
+<div class="ui-grid-a main-button">
+	<div class="ui-block-a">
+		<a href="go.html"><button type="submit" value="order">Order a Cab</button></a>
+	</div>
+</div>
+<div class="ui-grid-a">
+	<div class="ui-block-a">
+		<button type="submit" value="history">History</button>
+	</div>
+	<div class="ui-block-b">
+		<button type="submit" value="profile">Profile</button>
+	</div>
+</div>
+<?
+require('inc/footer.php');
+?>
